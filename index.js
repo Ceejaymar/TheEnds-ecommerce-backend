fba13ec5f3d1698ba8bfd459181fb591edbb951d
Carlos Martinez <martinezcj2@gmail.com>
2019-03-15 00:12:55 -0400

    Build express server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const router = require('./routes');
+const port = 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', router);
+
+app.use((err, req, res, next) => {
+  res.status(400).json({error: err.toString()});
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port: ${port}`);
+});
