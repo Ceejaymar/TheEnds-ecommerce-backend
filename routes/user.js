244ff51c6063e015e5d7a5c2b9d09e166c4fe033
Carlos Martinez <martinezcj2@gmail.com>
2019-03-15 19:38:36 -0400

    Add update and delete routes and services

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,22 @@ const express = require('express');
 const userRouter = express.Router();
 const UserService = require('../services/user');
 
-userRouter.get('/', (req, res, next) => {
-  UserService.read()
+userRouter.post('/', (req, res, next) => {
+  const { fname, lname, username, email, password, address, city, state, zipcode, seller } = req.body;
+
+  UserService.create(fname, lname, username, email, password, address, city, state, zipcode, seller)
+    .then(data => {
+      res.json(`Success: New user created with ID: ${data.id}`);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+userRouter.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  UserService.read(id)
     .then(data => {
       res.json(data);
     })
@@ -12,6 +26,37 @@ userRouter.get('/', (req, res, next) => {
     });
 });
 
+userRouter.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { fname, lname, username, email, password, address, city, state, zipcode, seller } = req.body;
+  
+  UserService.update(id, fname, lname, username, email, password, address, city, state, zipcode, seller)
+    .then(data => {
+      res.json('Success: Profile has been updated');
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+userRouter.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  UserService.delete(id)
+    .then(data => {
+      res.json( "Success: Your account has been deleted");
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// userRouter.get('/:id', (req, res, next) => {
+//   UserService.read
+// })
+
+
 // get all orders from user
+// 
 
 module.exports = userRouter;
--- services/user.js
+++ services/user.js
@@ -1,12 +1,54 @@
 const { db } = require('./dbConnect');
 const UserService = {};
 
-UserService.read = () => {
-  const sql =`
+UserService.create = (fname, lname, username, email, password, address, city, state, zipcode, seller) => {
+  const sql = `
+    INSERT INTO
+      users (fname, lname, username, email, password, address, city, state, zipcode, seller)
+    VALUES
+      ($[fname], $[lname], $[username], $[email], $[password], $[address], $[city], $[state], $[zipcode], $[seller])  
+    RETURNING id;  
+  `;
+  return db.one(sql, { fname, lname, username, email, password, address, city, state, zipcode, seller })
+}
+
+UserService.read = (id) => {
+  const sql = `
     SELECT *
     FROM users
+    WHERE
+      users.id = $[id]
   `;
-  return db.any(sql)
+  return db.one(sql, { id })
+}
+
+// get all orders for particular user
+
+UserService.update = (id, fname, lname, username, email, password, address, city, state, zipcode, seller) => {
+  const sql = `
+    UPDATE users
+    SET
+      fname = $[fname], 
+      lname = $[lname], 
+      username = $[username], 
+      email = $[email], 
+      password = $[password], 
+      address = $[address], 
+      city = $[city],
+      state = $[state], 
+      zipcode = $[zipcode], 
+      seller = $[seller]
+    WHERE 
+      users.id = $[id]
+  `;
+  return db.none(sql, { id, fname, lname, username, email, password, address, city, state, zipcode, seller })
+}
+
+UserService.delete = (id) => {
+  const sql =  `
+    DELETE FROM users WHERE id = $[id]
+  `;  
+  return db.none(sql, { id });
 }
 
 module.exports = UserService;
