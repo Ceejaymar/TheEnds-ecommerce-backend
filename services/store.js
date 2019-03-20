902c9a510ac27477edde06612fa1bd0ed6c048c8
Carlos Martinez <martinezcj2@gmail.com>
2019-03-20 14:27:32 -0400

    Create get product service && add get order notes

--- services/store.js
+++ services/store.js
@@ -1,11 +1,36 @@
-const { db } = './dbConnect.js';
+const { db } = require('./dbConnect.js');
 const StoreService = {};
 
-StoreService.read = (id) => {
+StoreService.getStoreProducts = (id) => {
   const sql = `
-    SELECT *
+    SELECT 
+      products.*
     FROM stores
+    JOIN products
+      ON stores.id = products.store_id
+    WHERE
+      stores.id = $[id]
   `;
-  return db.any(sql);
+  return db.any(sql, { id });
 }
 
+
+//I want to get all order for store id that I pass in
+//Products have the reference to a store ID
+//Orderline has a reference to the products id
+//Orderline also has reference to Order id
+//I would have to make some stor of join with products, order and orderling to get orders for specific store
+StoreService.getStoreOrders = (id) => {
+  const sql = `
+    SELECT 
+      orders.*
+    FROM orders
+    JOIN orderline
+      ON orders.id = orderline.order_id  
+    WHERE
+      .id = $[id]
+  `;
+  return db.any(sql, { id });
+}
+
+module.exports = StoreService;
