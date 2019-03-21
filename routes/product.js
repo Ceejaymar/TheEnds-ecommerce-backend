7e87b21e7ab66e3614218f5343c1496af17c573a
Carlos Martinez <martinezcj2@gmail.com>
2019-03-21 02:42:22 -0400

    Create get, put & delete routes

--- routes/product.js
+++ routes/product.js
@@ -3,3 +3,42 @@ const productRouter = express.Router();
 const ProductService = require('../services/product');
 
 // Get product info 
+productRouter.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  ProductService.read(id)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// Update product
+productRouter.put('/id', (req, res, next) => {
+  const { id } = req.params;
+
+  ProductService.update(id)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// Delete product
+productRouter.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  ProductService.delete(id)
+    .then(data => {
+      res.json("Success: user deleted");
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = productRouter;
