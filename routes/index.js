66c43b1c5315cbebf5813ef90a7715cc3cf1020b
Carlos Martinez <martinezcj2@gmail.com>
2019-03-21 02:40:11 -0400

    Add path to product routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = express();
 router.use('/user', require('./user'));
 router.use('/order', require('./order'));
 router.use('/store', require('./store'));
+router.use('/product', require('./product'));
 
 module.exports = router;
