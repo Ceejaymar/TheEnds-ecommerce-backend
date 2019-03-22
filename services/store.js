6b011e71b75fb5b07f18cdf1a05b023fef73db43
Carlos Martinez <martinezcj2@gmail.com>
2019-03-22 00:47:07 -0400

    Write SQL query for getting store orders

--- services/store.js
+++ services/store.js
@@ -14,20 +14,18 @@ StoreService.getStoreProducts = (id) => {
   return db.any(sql, { id });
 }
 
-//I want to get all order for store id that I pass in
-//Products have the reference to a store ID
-//Orderline has a reference to the products id
-//Orderline also has reference to Order id
-//I would have to make some stor of join with products, order and orderling to get orders for specific store
+// Getting duplicate order because they have multiple items in them
 StoreService.getStoreOrders = (id) => {
   const sql = `
     SELECT 
       orders.*
-    FROM orders
+    FROM stores
     JOIN orderline
-      ON orders.id = orderline.order_id  
+      ON stores.id = orderline.store_id
+    JOIN orders
+      ON orders.id = orderline.order_id
     WHERE
-      .id = $[id]
+      orderline.store_id = $[id]
   `;
   return db.any(sql, { id });
 }
