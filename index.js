b238e2136d4d35e6a32353e5ada1d8cd6a9315af
Carlos Martinez <martinezcj2@gmail.com>
2019-03-21 23:04:51 -0400

    Change port to 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const router = require('./routes');
-const port = 3001;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
