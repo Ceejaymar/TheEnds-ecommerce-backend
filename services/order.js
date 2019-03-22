bda22e79d7966682f497f0571f2ff528757299eb
Carlos Martinez <martinezcj2@gmail.com>
2019-03-22 13:57:45 -0400

    Create order update service & Start create order service

--- services/order.js
+++ services/order.js
@@ -18,4 +18,28 @@ OrderService.read = (id) => {
   return db.any(sql, { id });
 };
 
+OrderService.create = (customer, total, status) => {
+  const sql = `
+    INSERT INTO 
+      orders (customer, total, status)
+    VALUES
+      ($[customer], $[total], $[status])
+    RETURNING id;
+  `;
+  return db.one(sql, { customer, total, status });
+};
+
+OrderService.update = (id, status) => {
+  const sql = `
+    UPDATE
+      orders
+    SET
+      status = $[status],
+      updatedat = NOW()
+    WHERE
+      orders.id = $[id]
+  `;
+  return db.none(sql, { id, status })
+};
+
 module.exports = OrderService;
