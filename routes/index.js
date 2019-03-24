1714fe36501eb106fde84c4db758e60565255f60
Carlos Martinez <martinezcj2@gmail.com>
2019-03-23 22:37:56 -0400

    Add general route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express();
 
+router.get('/', (req, res, next) => {
+  res.json('Welcome to The Ends');
+});
+
 router.use('/user', require('./user'));
 router.use('/order', require('./order'));
 router.use('/store', require('./store'));
