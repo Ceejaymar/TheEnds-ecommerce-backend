8fee2a4d2bc598e0ec16fb264f37c6de61c535dd
Carlos Martinez <martinezcj2@gmail.com>
2019-03-24 02:52:36 -0400

    Import CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const router = require('./routes');
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
