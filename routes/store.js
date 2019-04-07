590bf6b1271c1ac696ac1437074f5b362cf8dd74
Carlos Martinez <martinezcj2@gmail.com>
2019-04-07 02:46:31 -0400

    Create list all stores route

--- routes/store.js
+++ routes/store.js
@@ -2,6 +2,20 @@ const express = require('express');
 const storeRouter = express.Router();
 const StoreService = require('../services/store');
 
+// Update store info
+storeRouter.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name, address, city, state, zipcode } = req.body;
+
+  StoreService.update(id, name, address, city, state, zipcode)
+    .then(() => {
+      res.json("Success: Store updated");
+    })
+    .catch(err => {
+      next(err);
+    });
+  });
+
 // Get all products for one store
 storeRouter.get('/:id/products', (req, res, next) => {
   const { id } = req.params;
@@ -28,18 +42,15 @@ storeRouter.get('/:id/orders', (req, res, next) => {
     });
 });
 
-// Update store info
-storeRouter.put('/:id', (req, res, next) => {
-  const { id } = req.params;
-  const { name, address, city, state, zipcode } = req.body;
-
-  StoreService.update(id, name, address, city, state, zipcode)
-    .then(() => {
-      res.json("Success: Store updated");
+// Get list of all stores
+storeRouter.get('/all', (req, res, next) => {
+  StoreService.getStoreList()
+    .then(data => {
+      res.json(data);
     })
     .catch(err => {
       next(err);
     });
 });
 
-module.exports = storeRouter;
+  module.exports = storeRouter;
