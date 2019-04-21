734ccf6e76fc4b2b63c97fe07cac4c072d87fe94
Carlos Martinez <martinezcj2@gmail.com>
2019-04-21 03:45:13 -0400

    Change port number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./routes');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
