e5024a8cf4cb5798bb8456a220f3ce2fb526da8d
Ceejaymar <martinezcj2@gmail.com>
2019-11-09 22:47:43 -0500

    Create stripe checkout route

---
+++ routes/stripeCheckout.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const uuid = require('uuid');
+const stripeRouter = express.Router();
+
+stripeRouter.post('/', async (req, res) => {
+  console.log("Request:", req.body);
+
+  let error;
+  let status;
+
+  try {
+    //todo: change product to total amount
+    const { token, product} = req.body;
+
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    });
+
+    const idempotency_key = uuid();
+    const charge = await stripe.charges.create({
+      amount: product.price * 100, // todo: change product to toatl
+      currency: "usd",
+      customer: customer.id,
+      receipt_email: token.email,
+      description: `Purchased from The Ends`,
+      shipping: {
+        name: token.card.name,
+        address: {
+          line1: token.card.address_line1,
+          line2: token.card.address_line2,
+          city: token.card.address_city,
+          country: token.card.address_country,
+          postal_code: token.card.address_zip
+        }
+      }
+    },
+    {
+      idempotency_key
+    });
+
+    console.log("Charge:", { charge });
+    status = "success";
+  }
+  catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+
+  res.json({ error, status });
+});
+
+module.exports = stripeRouter;
